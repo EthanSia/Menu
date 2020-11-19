471793e5f7369a0f12c93f7d9445e40e355384eb
Ethan <D00225319@student.dkit.ie>
2020-11-19 12:14:59 +0000

    SET UP

---
+++ src/main/java/com/dkit/Sd2a/ethan/sia/App.java
@@ -0,0 +1,13 @@
+package com.dkit.Sd2a.ethan.sia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/dkit/Sd2a/ethan/sia/MenuOption.java
@@ -0,0 +1,7 @@
+package com.dkit.Sd2a.ethan.sia;
+
+public enum MenuOption
+{
+    COUNTINUE, CREATE_BANK, ADD_BRANCH, ADD_CUSTOMER,MAKE_TRANSACTION,
+    DISPLAY_INFORMATION,QUIT;
+}
